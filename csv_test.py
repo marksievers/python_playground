d03576a0895053b5a23d6f3b92e863d69bb6b247
Mark Sievers <mark.sievers@gmail.com>
2013-02-04 12:20:49 +1300

    Add csv tests

--- csv_test.py
+++ csv_test.py
@@ -2,6 +2,7 @@ import os
 import sys
 import csv
 from os.path import basename
+from tempfile import TemporaryFile
 from sys import argv
 import re
 import time
@@ -10,8 +11,17 @@ import traceback
 import copy
 
 def main():
-    
-    reader = csv.DictReader(open('csv_test.csv'))
+
+
+    #This works for dirty windows files
+    # tf = TemporaryFile('b+wU')
+    # for chunk in request.FILES['branch_file'].chunks():
+    #   tf.write(chunk)
+    # tf.seek(0)
+    # csv_reader = reader(tf)
+
+
+    reader = csv.DictReader(open('APN.csv', 'rU'))
     for row_dict in reader:
         print "Operating on ", row_dict
         for k, v in row_dict.iteritems():
@@ -19,3 +29,11 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# http://stackoverflow.com/questions/1875956/how-can-i-access-an-uploaded-file-in-universal-newline-mode
+# http://stackoverflow.com/questions/3107793/python-script-reading-from-a-csv-file
+# http://docs.python.org/2/tutorial/inputoutput.html#reading-and-writing-files
+# http://docs.python.org/2/library/functions.html#open
+# http://selfsolved.com/problems/python-csv-and-universal-newline-mode
+# http://docs.python.org/2/library/csv.html#csv-fmt-params
+# http://www.gossamer-threads.com/lists/python/dev/723649
